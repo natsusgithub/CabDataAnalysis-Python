815fcd913e934dc9e615c81272f07353f9d43d5b
natsusgithub <natsus@gmail.com>
2016-08-06 15:06:54 -0400

    added neighborhoods from file

--- cabtrip/models.py
+++ cabtrip/models.py
@@ -9,6 +9,8 @@ import sys
 from peewee import Model, SqliteDatabase, InsertQuery,\
                    IntegerField, CharField, DoubleField, DateTimeField
 
+from geopy.geocoders import Nominatim
+
 from . import config
 
 
@@ -47,21 +49,15 @@ class TypePayment(BaseModel):
     # get one row
     def find(self, code):
         return self.get(self.code == code)
+
+class Neighborhood(BaseModel):
+    city = CharField()
+    state = CharField()
+    lat = DoubleField()
+    long = DoubleField()
+    zipcode = CharField()
     
 class CabTrip(BaseModel):
-    # properties for demo purposes we would load the following
-    # id
-    # pickuplong
-    # pickuplat
-    # dropofflong
-    # dropofflat
-    # ridedistance
-    # pickuptime
-    # dropofftime
-    # numpassenger
-    # paymenttype
-    # tipamount
-    # fareamount
     pickup_time = DateTimeField()
     pickup_long = DoubleField()
     pickup_lat = DoubleField()
@@ -110,7 +106,28 @@ def convert_str_to_date(item):
         return datetime.strptime(item, "%Y-%m-%d %H:%M:%S")
     #return retdate
 
+def update_neighborhood():
+    for n in Neighborhood.select():
+        (CabTrip
+        .update(pickup_neighborhood = n.zipcode)
+        .where(
+            n.lat == str(CabTrip.pickup_lat)[:5],
+            n.long == str(CabTrip.pickup_long)[:6]
+         )).execute()
+        (CabTrip
+        .update(dropoff_neighborhood = n.zipcode)
+        .where(
+            n.lat == str(CabTrip.dropoff_lat)[:5],
+            n.long == str(CabTrip.dropoff_long)[:6]
+         )).execute()
 
+def get_neighborhood(lat, long):
+    try:
+        for n in Neighborhood.select().where((str(Neighborhood.lat) in lat), (str(Neighborhood.long) in long)):
+            return n.city
+    except ValueError:
+        return None        
+    return None
 
 # this will be configurable where we can pass arguments on whether to reload
 # the data or to use whatever is in Sqlite
@@ -118,28 +135,43 @@ def load_data(db):
     cabs = []
     index = 1
     starttime = time.time()
-    with open('cabdata_all.csv') as csvfile:
-        datareader = csv.reader(csvfile)
+    neighborhoods = []
+    print("loading neighborhood data...")
+    with open('zipcode.csv') as zipcodefile:
+        datareader = csv.reader(zipcodefile, delimiter=',', quotechar='"')
+        with db.atomic():
+            for row in datareader:
+                # only care about NY
+                if (len(row) > 0):
+                    if (row[2] == 'NY'):
+                        neighborhoods.append({"city": row[1], \
+                                              "state": row[2], \
+                                              "zipcode": row[0], \
+                                              "lat": row[3], \
+                                              "long": row[4]})
+                        
+                        if (index % 100 == 0):
+                            Neighborhood.insert_many(neighborhoods).execute()
+                            neighborhoods = []
+                        
+                        if (((time.time() - starttime) % 60.0) > 1):
+                            __progressbar(index)
+                            starttime = time.time()
+                        index += 1
+        if (len(neighborhoods) > 0):
+            Neighborhood.insert_many(neighborhoods).execute()
+    __progressbar(index)
 
-        next(datareader)
+    index = 1
+    print("loading cab data...")
+    with open('cabdata.csv') as csvfile:
+        datareader = csv.reader(csvfile)
+        next(datareader) # skip header row
         # speeds up the insert process significantly
         cabtrips = []
         with db.atomic():
             for row in datareader:
-                '''
-                CabTrip.insert(pickup_time = convert_str_to_date(row[1]), \
-                               pickup_lat = row[6], \
-                               pickup_long = row[5], \
-                               dropoff_time = convert_str_to_date(row[2]), \
-                               dropoff_lat = row[10], \
-                               dropoff_long = row[9], \
-                               ride_distance = row[4], \
-                               num_passenger = row[3], \
-                               payment_type = row[11], \
-                               tip_amount = row[15], \
-                               fare_amount = row[12], 
-                               ).execute()
-                '''
+                # to speed up inserts, we create an array first and insert a giant list of it in bulk
                 cabtrips.append({"pickup_time": convert_str_to_date(row[1]), \
                                "pickup_lat": row[6], \
                                "pickup_long": row[5], \
@@ -150,20 +182,28 @@ def load_data(db):
                                "num_passenger": row[3], \
                                "payment_type": row[11], \
                                "tip_amount": row[15], \
-                               "fare_amount": row[12]})
-                if (index % 90 == 0):
+                               "fare_amount": row[12], \
+                               #"pickup_neighborhood": get_neighborhood(row[6],row[5])
+                                 })
+                               
+                if (index % 80 == 0):
                     CabTrip.insert_many(cabtrips).execute()
                     cabtrips = []
                     
-                index += 1
                 if (((time.time() - starttime) % 60.0) > 1):
                     __progressbar(index)
                     starttime = time.time()
-        
+                index += 1
+        if (len(cabtrips) > 0):
+            CabTrip.insert_many(cabtrips).execute()
     __progressbar(index)
     print('completed!')
+    print('updating neighborhoods...')
+    #update_neighborhood()
+    
+    
 
 def create_tables(db):
     db.connect()
-    db.create_tables([CabTrip,TypePayment], safe=True)
+    db.create_tables([CabTrip,TypePayment,Neighborhood], safe=True)
     db.close()
